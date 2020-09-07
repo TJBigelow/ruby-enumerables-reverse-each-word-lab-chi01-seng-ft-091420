5e23fbbc122c5ffbc94db329ce2d3dde428dc850
Thomas Bigelow <tjbigelow@att.net>
2020-09-07 20:39:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split(' ')
   reverse_array = []
-  string_array.each {|word| 
+  string_array.each {|word|
     rev_word = ""
     word.split(//).reverse_each {|letter| rev_word += letter}
     reverse_array.push(rev_word)
